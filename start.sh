625a69aaf7fb87b20b3ee909a9c29aa242179660
VîVeK RøY <83235714+unknownforall1@users.noreply.github.com>
2021-10-21 07:03:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo..."
-git clone https://github.com/ImJanindu/VC_PLAYER.git /VC_PLAYER
+git clone https://github.com/unknownforall1/VC_PLAYER.git /VC_PLAYER
 cd /VC_PLAYER
 pip3 install -U -r requirements.txt
 echo "Starting Bot..."
